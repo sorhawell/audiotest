c98c85196838917bbe297bbd118768bb88f2d0fa
daniellga <43149605+daniellga@users.noreply.github.com>
2022-08-23 16:52:14 -0300

    Update tests.R

--- src/rust/src/tests.R
+++ src/rust/src/tests.R
@@ -1,2 +1,6 @@
 rextendr::document()
 devtools::load_all(".")
+
+audio = audior::load("../../../test_files/mono.wav")
+sr = audior::get_samplerate("../../../test_files/mono.wav")
+audior::play(audio, sr)
