0ce525aa7ce0bcd141638f060fba65b7592a3c62
daniellga <43149605+daniellga@users.noreply.github.com>
2022-08-23 16:53:18 -0300

    Update tests.R

--- src/rust/src/tests.R
+++ src/rust/src/tests.R
@@ -1,6 +1,6 @@
 rextendr::document()
 devtools::load_all(".")
 
-audio = audior::load("../../../test_files/mono.wav")
+audio = audior::load("../../../test_files/mono.wav") # decode audio into R Matrix
 sr = audior::get_samplerate("../../../test_files/mono.wav")
 audior::play(audio, sr)
